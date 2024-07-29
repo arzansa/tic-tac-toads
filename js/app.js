f8efb2d0ad1d3e38ae198d185b13013e92e794a4
arzansa <arzansa@gmail.com>
2024-07-28 20:27:59 -0500

    Added logic for attacking, blocking, and evading (win/lose/tie)

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 /*-------------------------------- Constants --------------------------------*/
 const startButton = document.getElementById('startButton');
 const titleEls = document.querySelectorAll('.title');
-const board = document.getElementById('board');
+const gameBoard = document.getElementById('board');
 const textBox = document.getElementById('textBox');
 const continueButton = document.getElementById('continueButton');
 const hpBars = document.querySelectorAll('.hp-bar');
 const dialogues = ['You will need to find the missing parts of your ship to return home.',
-    'As you venture out into the strange land, you encounter a giant toad!',
+    'As you venture out into the strange land, you encounter a giant toad! He snarls menacingly...',
     'As you draw your weapon, he begins drawing something in the sand.',
     'He wants to play... Tic Tac Toe?',
     'This is the game board. When it is your attack turn, you will play first.',
@@ -14,7 +14,7 @@ const dialogues = ['You will need to find the missing parts of your ship to retu
     'Each round of Tic Tac Toe has 3 outcomes.',
     'You win: You do full damage to the toad.',
     'Tie: The toad blocks some of your damage.',
-    'Toad wins: You fumble, dealing no damage.',
+    'Toad wins: The toad evades your attack, dealing no damage.',
     "Got it? Good! Let's begin. Choose your first move!"
 ];
 const gameText = document.getElementById('gameText');
@@ -22,18 +22,83 @@ const gameText = document.getElementById('gameText');
 /*---------------------------- Variables (state) ----------------------------*/
 let onTitleScreen = true;
 let textIdx = 0;
+let playerHP;
+let enemyHP;
+let playerAtk = 1;
+let enemyAtk = 1;
+let playerBlk = 1;
+let enemyBlk = 1;
+
+// variables defined by lab requirements
+let board;
+let turn;
+let winner;
+let tie;
+let playerAttacking;
 
 /*------------------------ Cached Element References ------------------------*/
-
+const squareEls = document.querySelectorAll('.sqr');
+const messageEl = document.getElementById('message');
 
 
 /*-------------------------------- Functions --------------------------------*/
+function init() {
+    board = ['', '', '', '', '', '', '', '', ''];
+    turn = 'X';
+    playerAttacking = true;
+    winner = false;
+    tie = false;
+    render();
+}
+
+function render() {
+
+}
+
+function updateBoard() {
+    board.forEach((square, sqrIdx) => {
+        if (squareEls[sqrIdx] === '') {
+            square.innerText = 'A';
+        }
+    });
+}
+
+function updateMessage() {
+    if (winner === false && tie === false) {
+        if (turn === 'X') {
+            messageEl.innerText = 'Your turn';
+        } else if (turn === 'O') {
+            messageEl.innerText = "Toad's turn";
+        }
+    } else if (winner === false && tie === true) {
+        if (playerAttacking) {
+            messageEl.innerText = `You tied! You deal ${playerAtk - enemyBlk} damage (${enemyBlk} damage blocked by the toad).`;
+        } else {
+            messageEl.innerText = `You tied! The toad deals ${enemyAtk - playerBlk} damage (${playerBlk} damage blocked by you).`;
+        }
+    } else {
+        if (playerAttacking) {
+            if (turn === 'X') {
+                messageEl.innerText = `You won! You deal ${playerAtk} damage!`;
+            } else if (turn === 'O') {
+                messageEl.innerText = `Toad won! You miss, dealing 0 damage...`;
+            }
+        } else {
+            if (turn === 'X') {
+                messageEl.innerText = `Toad wins! You take ${enemyAtk} damage. Oof!!!`;
+            } else if (turn === 'O') {
+                messageEl.innerText = `You win! You evade the toad's attack completely.`;
+            }
+        }
+    }
+}
+
 function startGame() {
     textBox.style.display = 'flex';
 }
 
 function showBoard() {
-    board.style.display = 'flex';
+    gameBoard.style.display = 'flex';
 }
 
 function showHP() {
@@ -71,6 +136,8 @@ continueButton.addEventListener('click', function() {
         gameText.innerText = dialogues[textIdx];
         textIdx++;
         continueButton.style.display = 'none';
+        init();
+        messageEl.style.display = 'block';
     }
 });
 
