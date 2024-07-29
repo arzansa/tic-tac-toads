909b6e3adffbba1aa747990030beb53d7ef90866
arzansa <arzansa@gmail.com>
2024-07-28 19:16:22 -0500

    Finished dialogue up to first move, HP bars added

--- js/app.js
+++ js/app.js
@@ -4,12 +4,18 @@ const titleEls = document.querySelectorAll('.title');
 const board = document.getElementById('board');
 const textBox = document.getElementById('textBox');
 const continueButton = document.getElementById('continueButton');
+const hpBars = document.querySelectorAll('.hp-bar');
 const dialogues = ['You will need to find the missing parts of your ship to return home.',
     'As you venture out into the strange land, you encounter a giant toad!',
     'As you draw your weapon, he begins drawing something in the sand.',
-    'He wants to play... Tic Tac Toe?'
-];
-const tutorialDialogues = [
+    'He wants to play... Tic Tac Toe?',
+    'This is the game board. When it is your attack turn, you will play first.',
+    "You and the enemy toad both have hit points. The first to 0 loses the battle. If you lose, it's game over!",
+    'Each round of Tic Tac Toe has 3 outcomes.',
+    'You win: You do full damage to the toad.',
+    'Tie: The toad blocks some of your damage.',
+    'Toad wins: You fumble, dealing no damage.',
+    "Got it? Good! Let's begin. Choose your first move!"
 ];
 const gameText = document.getElementById('gameText');
 
@@ -30,6 +36,12 @@ function showBoard() {
     board.style.display = 'flex';
 }
 
+function showHP() {
+    hpBars.forEach((hpBar) => {
+        hpBar.style.display = 'inline';
+      });
+}
+
 
 /*----------------------------- Event Listeners -----------------------------*/
 startButton.addEventListener('click', function() {
@@ -42,13 +54,23 @@ startButton.addEventListener('click', function() {
 });
 
 continueButton.addEventListener('click', function() {
-    if(dialogues[textIdx]) {
+    if(textIdx < 4) {
         gameText.innerText = dialogues[textIdx];
         textIdx++;
-    } else {
+    } else if (textIdx === 4) {
         showBoard();
-        gameText.innerText = 'This is the game board. When it is your attack turn, you will play first.';
-
+        gameText.innerText = dialogues[textIdx];
+        textIdx++;
+    } else if (textIdx > 4 && textIdx < 10) {
+        gameText.innerText = dialogues[textIdx];
+        if (textIdx === 5) {
+            showHP();
+        }
+        textIdx++;
+    } else if (textIdx === 10) {
+        gameText.innerText = dialogues[textIdx];
+        textIdx++;
+        continueButton.style.display = 'none';
     }
 });
 
